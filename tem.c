124da89693f36f0b1bcaa7b07b564800c0fe9a6e
faineance <faineance@users.noreply.github.com>
2015-04-29 18:42:52 +0100

    refactor and add additional keybinds

--- tem.c
+++ tem.c
@@ -4,19 +4,44 @@
 #include "config.h"
 
 
+static gboolean do_copy (VteTerminal *terminal)
+{
+	vte_terminal_copy_clipboard (terminal);
+	return TRUE;
+}
+
+static gboolean do_paste (VteTerminal *terminal)
+{
+	vte_terminal_paste_clipboard (terminal);
+	return TRUE;
+}
+
+static gboolean do_quit ()
+{
+	gtk_main_quit ();
+	return TRUE;
+}
+
 static gboolean on_key_press(GtkWidget *window, GdkEventKey *event, gpointer user_data)
 {
 	if (event->type != GDK_KEY_PRESS)
 		return FALSE;
+
 	VteTerminal *terminal = VTE_TERMINAL (user_data);
-	if ((event->state & GDK_CONTROL_MASK) && (event->state & GDK_SHIFT_MASK)) {
-		switch (event->keyval) {
-			case GDK_KEY_C:
-			vte_terminal_copy_clipboard (terminal);
-			return TRUE;
-			case GDK_KEY_V:
-			vte_terminal_paste_clipboard (terminal);
-			return TRUE;
+
+	if (event->state & GDK_CONTROL_MASK)
+	{
+		if (event->state & GDK_SHIFT_MASK)
+		{
+			switch (event->keyval) 
+			{
+				case GDK_KEY_C:
+					return do_copy(terminal);
+				case GDK_KEY_V:
+					return do_paste(terminal);
+				case GDK_KEY_Q:
+					return do_quit();
+			}
 		}
 	}
 	return FALSE;
@@ -24,6 +49,7 @@ static gboolean on_key_press(GtkWidget *window, GdkEventKey *event, gpointer use
 }
 
 int main(int argc, char **argv) {
+
 	gtk_init(&argc, &argv);
 
 	GtkWindow *window = GTK_WINDOW(gtk_window_new(GTK_WINDOW_TOPLEVEL));
@@ -31,10 +57,10 @@ int main(int argc, char **argv) {
 	gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(vte));
 
 
-	g_signal_connect(window, "delete_event", gtk_main_quit, NULL);
+	g_signal_connect(window, "delete_event", G_CALLBACK(do_quit), NULL);
 
 	g_signal_connect(G_OBJECT(window), "key-press-event", G_CALLBACK(on_key_press), vte);
-	g_signal_connect(vte, "child-exited", gtk_main_quit, NULL);
+	g_signal_connect(vte, "child-exited", G_CALLBACK(do_quit), NULL);
 
 	char *shell[2] = {SHELL, NULL};
 	vte_terminal_spawn_sync(vte, 0, NULL, shell, NULL, 0, NULL, NULL, NULL, NULL, NULL);
